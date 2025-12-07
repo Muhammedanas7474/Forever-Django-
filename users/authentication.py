6c9bf8c0b182216b7e1c5721f7d3e9883661a61f
anas <muhammedanas247474@gmail.com>
2025-12-07 19:08:17 +0530

    completed

--- users/authentication.py
+++ users/authentication.py
@@ -1,15 +1,21 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.exceptions import AuthenticationFailed
 
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
         access_token = request.COOKIES.get("access_token")
 
         if not access_token:
-            return None  
+            return None
 
         try:
             validated_token = self.get_validated_token(access_token)
             user = self.get_user(validated_token)
+
+            if user.blocked:
+                raise AuthenticationFailed("Your account has been blocked")
+
             return (user, validated_token)
+
         except Exception:
             return None
